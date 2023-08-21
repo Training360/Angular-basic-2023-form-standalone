c665dbe71129bace093a7f1c9a478af46826ab9f
cherryApp <j.cserko@gmail.com>
2023-08-21 13:45:38 +0200

    start lesson-09

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,24 +14,9 @@ const routes: Routes = [
   },
   {
     path: 'ticket',
-    children: [
-      {
-        path: '',
-        pathMatch: 'full',
-        loadComponent: () => import('./ticket/tickets/tickets.component').then(
-          m => m.TicketsComponent),
-      },
-      {
-        path: 'edit/:id',
-        loadComponent: () => import('./ticket/ticket-editor/ticket-editor.component').then(
-          m => m.TicketEditorComponent)
-      },
-      {
-        path: 'create',
-        loadComponent: () => import('./ticket/ticket-create/ticket-create.component').then(
-          m => m.TicketCreateComponent)
-      },
-    ]
+    loadChildren: () => import('./ticket/routes').then(
+      mod => mod.TICKET_ROUTES
+    ),
   },
   {
     path: '**',
